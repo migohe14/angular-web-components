f3192aa0aef0cbca8a44e1a6aec49bef5246bbaf
migohe14 <migohe14@gmail.com>
2020-10-23 22:39:11 +0200

    web component - hamburger menu

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import '@dile/dile-menu-hamburger/dile-menu-hamburger.js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'web-components-angular';
+  menuOpened = false;
+
+  openMenu() {
+    this.menuOpened = true;
+  }
+
+  menuClosedHandler() {
+    this.menuOpened = false;
+  }
+  menuOpenedHandler() {
+    this.menuOpened = true;
+  }
+}
